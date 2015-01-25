5536548cc0dfc12275a72668a0824ce36bb2083e
skyline75489 <skyline75489@outlook.com>
2015-01-25 10:59:47 +0800

    math

---
+++ factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int result = 0;  
+        while(n)  
+        {  
+            result += n/5;  
+            n /= 5;  
+        }  
+        return result;  
+    }
+};
